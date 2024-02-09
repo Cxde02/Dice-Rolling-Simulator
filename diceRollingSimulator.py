a343b99cae7a5239fa7582e4c9d00064e77170f3
Ruhomaun Khaleelullah Ahmad Raza Qadri <105217847+Cxde02@users.noreply.github.com>
2024-02-09 09:15:32 +0400

    Update diceRollingSimulator.py

--- diceRollingSimulator.py
+++ diceRollingSimulator.py
@@ -42,22 +42,34 @@ def face6():
             '|*  *  *|',
             ' -------']
 
-print('---Dice Rolling Simulator---')
 
-# Randomly choose and print one of the faces
-random_face = random.choice([face1, face2, face3, face4, face5, face6])
-for line in random_face():
-    print(line)
+def roll_Dice():
+    # Randomly choose and print one of the faces
+    random_face = random.choice([face1, face2, face3, face4, face5, face6])
+    for line in random_face():
+        print(line)
 
-if random_face == face1:
-    print('You got 1')
-elif random_face == face2:
-    print('You got 2')
-elif random_face == face3:
-    print('You got 3')
-elif random_face == face4:
-    print('You got 4')
-elif random_face == face5:
-    print('You got 5')
-else:
-    print('You got 6')
+    if random_face == face1:
+        print('You got 1')
+    elif random_face == face2:
+        print('You got 2')
+    elif random_face == face3:
+        print('You got 3')
+    elif random_face == face4:
+        print('You got 4')
+    elif random_face == face5:
+        print('You got 5')
+    else:
+        print('You got 6')
+
+while True:
+    print('\n---Dice Rolling Simulator---')
+    print("Enter 'r' to Roll Dice or 'q' to Quit: ", end='')
+    userInput = input().lower()
+    if userInput == "r":
+        roll_Dice()
+    elif userInput == "q":
+        print('Exiting the program...')
+        break
+    else:
+        print('System accepts only \'r\' and \'q\'')
